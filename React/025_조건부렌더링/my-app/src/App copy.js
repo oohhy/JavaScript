b22e1f765154db73cf7e4152203d8f48ddc5f5ee
hyeyunOh <Ohhyoon0707@gmail.com>
2022-11-15 16:50:31 +0900

    11/15

---
+++ React/025_조건부렌더링/my-app/src/App copy.js
@@ -0,0 +1,12 @@
+function Hello({ name }) {
+  if (name) {
+    return <div>{name}</div>;
+  }
+  return <div>이름이 입력되지 않았습니다!</div>;
+}
+
+function App() {
+  return <Hello name="hojun" />;
+}
+
+export default App;
